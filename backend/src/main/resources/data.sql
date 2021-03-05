41d0220ae8c5598548b8adfda151af8a0056bc79
Alisson Youssf <youssfbr@gmail.com>
2021-03-05 18:17:28 -0300

    Seeding

---
+++ backend/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO tb_client (name, cpf, date_register) VALUES ('Alisson Youssf', '12345678', NOW());
+INSERT INTO tb_client (name, cpf, date_register) VALUES ('Marcio', '12345689', NOW());
+
+INSERT INTO tb_service (description, price, id_client) VALUES ('Desktop AOC', 100, 2);
+
